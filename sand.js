cefa04135edd40c3e6fba5f5b3fc5088dfca5f00
Anthony Cruz <anthonyleierlw@gmail.com>
2024-02-16 11:09:28 -0300

    Grãos com gravidade

--- sand.js
+++ sand.js
@@ -1,54 +1,81 @@
-class Matriz {
-    constructor(altura, largura) {
-        this.altura = altura;
-        this.largura = largura;
-        this.grade = this.criarMatriz(this.altura, this.largura);
-    }
-
-    criarMatriz(altura, largura) {
-        let matriz = [];
-        for (let i = 0; i < altura; i++) {
-            matriz[i] = [];
-            for (let j = 0; j < largura; j++) {
-                matriz[i][j] = 0;
-            }
-        }
-        return matriz;
-    }
+function copiarMatriz(matriz) {
+    return matriz.map((row) => row.slice());
 }
 
-function draw(canvas, matriz, tamanho) {
-    let contexto = canvas.getContext("2d");
-    for (i = 0; i < matriz.altura; i++) {
-        for (j = 0; j < matriz.largura; j++) {
-            if (matriz.grade[i][j] > 0) {
-                contexto.fillStyle = "red";
-                contexto.fillRect(i * tamanho, j * tamanho, tamanho, tamanho);
-            } else {
-                contexto.fillStyle = "yellow";
-                contexto.fillRect(i * tamanho, j * tamanho, tamanho, tamanho);
-            }
+function criarMatriz(altura, largura) {
+    let matriz = [];
+    for (let i = 0; i < altura; i++) {
+        matriz[i] = [];
+        for (let j = 0; j < largura; j++) {
+            matriz[i][j] = 0;
         }
     }
+    return matriz;
 }
 
-function main() {
-    const larguraCanvas = 100;
-    const alturaCanvas = 100;
-    const tamanho = 10;
-
-    let canvas = document.getElementById("GameCanvas");
-    canvas.width = larguraCanvas;
-    canvas.height = alturaCanvas;
-
-    const larguraMatriz = 10;
-    const alturaMatriz = 10;
-
-    matriz = new Matriz(alturaMatriz, larguraMatriz);
-
-    matriz.grade[1][1] = 1;
-    console.table(matriz.grade);
-    draw(canvas, matriz, tamanho);
+function updateMatriz(matriz) {
+    let novaMatriz = copiarMatriz(matriz);
+    for (i = 0; i < alturaMatriz; i++) {
+        for (j = 0; j < larguraMatriz; j++) {
+            if (matriz[i][j] > 0) {
+                if (i + 1 < alturaMatriz) {
+                    let embaixo = matriz[i + 1][j];
+                    if (embaixo === 0) {
+                        novaMatriz[i][j] = 0;
+                        novaMatriz[i + 1][j] = matriz[i][j];
+                    }
+                }
+            }
+        }
+    }
+    return novaMatriz;
 }
 
-main();
+function drawMatriz(matriz) {
+    contexto.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (i = 0; i < alturaMatriz; i++) {
+        for (j = 0; j < larguraMatriz; j++) {
+            if (matriz[i][j] > 0) {
+                contexto.fillStyle = "#C2A653";
+                contexto.fillRect(j * tamanho, i * tamanho, tamanho, tamanho);
+            }
+        }
+    }
+}
+
+const larguraCanvas = 400;
+const alturaCanvas = 400;
+const tamanho = 10;
+
+let canvas = document.getElementById("GameCanvas");
+let contexto = canvas.getContext("2d");
+canvas.width = larguraCanvas;
+canvas.height = alturaCanvas;
+
+const larguraMatriz = larguraCanvas / tamanho;
+const alturaMatriz = alturaCanvas / tamanho;
+
+let matriz = criarMatriz(alturaMatriz, larguraMatriz);
+
+canvas.addEventListener("click", (evento) => {
+    let x = Math.floor((evento.clientX - canvas.getBoundingClientRect().left) / tamanho);
+    let y = Math.floor((evento.clientY - canvas.getBoundingClientRect().top) / tamanho);
+    matriz[y][x] = 1;
+});
+
+function update() {
+    matriz = updateMatriz(matriz);
+}
+
+function draw() {
+    drawMatriz(matriz);
+}
+
+var FRAME = 1000 / 60;
+var loop = function () {
+    update();
+    draw();
+    setTimeout(loop, FRAME);
+};
+setTimeout(loop, FRAME);
