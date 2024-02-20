a454bfd5c939ee786c9a5ebdb0e3359e47bef3eb
Anthony Cruz <anthonyleierlw@gmail.com>
2024-02-20 15:05:27 -0300

    Formatação de código

--- src/entities/Matrix.js
+++ src/entities/Matrix.js
@@ -26,23 +26,20 @@ export default class Matrix {
             for (let j = 0; j < this.columns; j++) {
                 if (this.grid[i][j] !== null && !this.grid[i][j].fixed) {
                     if (i + 1 < this.lines) {
-                        let direction = 1;
-                        if (Math.random() < 0.5) {
-                            direction *= -1;
-                        }
+                        let direction = Math.random() < 0.5 ? -1 : 1;
 
                         let current = this.grid[i][j];
                         let below = this.grid[i + 1][j];
                         let belowA = this.grid[i + 1][j - direction];
                         let belowB = this.grid[i + 1][j + direction];
 
-                        if (below === null || (below != null && below.density < current.density && !below.fixed)) {
+                        if (below === null || (below !== null && below.density < current.density && !below.fixed)) {
                             newGrid[i][j] = below;
                             newGrid[i + 1][j] = current;
-                        } else if (belowA === null || (belowA != null && belowA.density < current.density && !belowA.fixed)) {
+                        } else if (belowA === null || (belowA !== null && belowA.density < current.density && !belowA.fixed)) {
                             newGrid[i][j] = belowA;
                             newGrid[i + 1][j - direction] = current;
-                        } else if (belowB === null || (belowB != null && belowB.density < current.density && !belowB.fixed)) {
+                        } else if (belowB === null || (belowB !== null && belowB.density < current.density && !belowB.fixed)) {
                             newGrid[i][j] = belowB;
                             newGrid[i + 1][j + direction] = current;
                         }
